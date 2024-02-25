04a6763cc76c14755e342d13b021b691e5f34e65
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2024-02-25 18:09:23 +0900

    [Feat] 페이지 구현

---
+++ src/views/pages/not-found/index.js
@@ -0,0 +1,9 @@
+import headerComponent from "../../components/header";
+import errorComponent from "../../components/error";
+
+const notFoundPage = () => {
+  headerComponent();
+  errorComponent();
+};
+
+export default notFoundPage;
