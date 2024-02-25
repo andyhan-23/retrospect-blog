d61e08d6c69b40a809c618cf4e8daefe66554dbb
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-02-25 13:45:27 +0900

    [Feat]라우터 구현 (#4)
    Co-authored-by: 한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>

---
+++ src/routers/index.js
@@ -0,0 +1,44 @@
+class Router {
+  constructor() {
+    this.routes = [];
+  }
+  /**
+   *
+   * @param {string} pattern - 특정 pattern을 문자열로 가져옵니다. 예: '/' , '/detail/(\\w+)"
+   * @param {function} page - 화면에 보여질 내용을 함수로 가져옵니다.
+   * @returns {new Router()}  - 생성할 라우터의 인스턴스를 반환합니다.
+   */
+  addRoute(pattern, page) {
+    const regexString = new RegExp(`^${pattern}$`);
+    this.routes.push({ regexString, page });
+    return this;
+  }
+  setNotFound(page) {
+    this.showNotFound = page;
+    return this;
+  }
+  navigate(path) {
+    let match;
+    const currentRoute = this.routes.find(value => {
+      match = path.match(value.regexString);
+      if (match) {
+        return true;
+      }
+    });
+    if (!match) {
+      this.showNotFound();
+      return;
+    }
+    currentRoute.page(match[1]);
+  }
+  start() {
+    window.addEventListener("DOMContentLoaded", () => {
+      this.navigate(window.location.pathname);
+    });
+    window.addEventListener("popstate", () => {
+      this.navigate(window.location.pathname);
+    });
+  }
+}
+
+export default Router;
