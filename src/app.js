9737d328812d86988f0afad72c2409d03336b6d9
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-02-25 17:23:50 +0900

    [Feat]리스트 컴포넌트 구현 (#8)
    Co-authored-by: 한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import headerComponent from "./views/components/header";
 import errorComponent from "./views/components/error";
+import listComponent from "./views/components/list";
+import Model from "./models";
 
-(() => {
-  const appTemplate = `
+const appTemplate = `
   <header id="header"></header>
   <main id="main"></main>
   <footer id="footer"></footer>
 `;
-  const app = document.getElementById("app");
-  app.innerHTML = appTemplate;
-})();
+const app = document.getElementById("app");
+app.innerHTML = appTemplate;
+const model = new Model();
 headerComponent();
 errorComponent();
+const list = async () => await model.mainList();
+listComponent(await list());
---
+++ src/views/components/list/index.js
@@ -0,0 +1,51 @@
+import "./index.css";
+import formatDate from "./format-date";
+
+const listTemplate = `
+  <main>
+    <div id="list_container">
+      <div id="list_inner_container">
+        <div id="title_container">
+          <h1>회고록</h1>
+        </div>
+        <ul id="article_list_container"></ul>
+      </div>
+    </div>
+  </main>  
+`;
+
+const listContainerRender = () => {
+  const main = document.getElementById("main");
+  main.innerHTML = listTemplate;
+};
+
+const articleTemplate = (list, id) => `
+  <article>
+    <section>
+      <div id="article_title_container">
+        <h1>${list[id].title}</h1>
+      </div>
+      <div id="article_summary_container">
+        <p>${list[id].summary}</p>
+      </div>
+      <div id="article_created_container">
+        <p>${formatDate(list[id].created_date)}</p>
+      </div>
+    </section>
+  </article>
+`;
+
+const listComponent = list => {
+  listContainerRender();
+  const listArr = Object.values(list);
+  console.log("확인", listArr);
+  listArr.reduceRight((acc, value) => {
+    const li = document.createElement("li");
+    li.id = value.id;
+    li.innerHTML = articleTemplate(list, value.id);
+    acc.appendChild(li);
+    return acc;
+  }, document.getElementById("article_list_container"));
+};
+
+export default listComponent;
