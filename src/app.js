c0fbcede572ad602a693be9e3034074c458a78d1
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-02-25 15:07:08 +0900

    [Feat] 헤더 컴포넌트 구현 (#6)
    Co-authored-by: 한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import headerComponent from "./views/components/header";
+
+(() => {
+  const appTemplate = `
+  <header id="header"></header>
+  <main id="main"></main>
+  <footer id="footer"></footer>
+`;
+  const app = document.getElementById("app");
+  app.innerHTML = appTemplate;
+})();
+headerComponent();
