6a611f004d0488db7bc733e51da5c71ebf30a093
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2024-02-26 14:20:33 +0900

    [Feat] 컨트롤러 구현

---
+++ src/controllers/main.js
@@ -0,0 +1,21 @@
+import mainPage from "../views/pages/main";
+import { addClickEventToItem } from "./click";
+import { addClickEventToLogo } from "./click";
+
+function MainController(router, model) {
+  this.mainPage = async () => {
+    try {
+      const list = await model.mainList();
+      mainPage(list);
+      addClickEventToItem(router);
+      addClickEventToLogo(router);
+    } catch (error) {
+      console.error(
+        `메인 컨트롤러를 통해 화면을 보여주는 부분에서 에러발생:${error}`,
+      );
+    }
+  };
+  router.addRoute("/", this.mainPage);
+}
+
+export default MainController;
