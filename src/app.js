987d505fa693563fffe6e132b7d3620dd726df1c
한재혁 <hanjaehyeok@hanjaehyeog-ui-MacBookPro.local>
2024-02-26 14:37:41 +0900

    [Feat] app 구현

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
-import headerComponent from "./views/components/header";
-import errorComponent from "./views/components/error";
-import listComponent from "./views/components/list";
-import contentComponet from "./views/components/content";
-import mainPage from "./views/pages/main";
-import detailPage from "./views/pages/detail";
-import notFoundPage from "./views/pages/not-found";
+import Router from "./routers";
 import Model from "./models";
+import initController from "./controllers";
+import "./index.css";
 
-const appTemplate = `
-  <header id="header"></header>
-  <main id="main"></main>
-  <footer id="footer"></footer>
+(() => {
+  const appTemplate = `
+  <div id="header"></div>
+  <div id="main"></div>
+  <div id="footer"></div>
 `;
-const app = document.getElementById("app");
-app.innerHTML = appTemplate;
-const model = new Model();
-//headerComponent();
-//errorComponent();
-const list = async () => await model.mainList();
-const content = async () => await model.detailItem(2);
-
-//detailPage(await content());
-//contentComponet(await content());
-notFoundPage();
-mainPage(await list());
+  const app = document.getElementById("app");
+  app.innerHTML = appTemplate;
+  const router = new Router();
+  const model = new Model();
+  const hasController = initController(router, model);
+  if (hasController) {
+    router.start();
+  }
+})();
